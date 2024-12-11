d11a244bc52961dcb8328d30e6d7d6afaba4e97c
Vincent Lohse <vincent.lohse@logicalclocks.com>
2024-12-11 22:49:35 +0100

    Using local namespace variable

--- test_scripts/test_backup_restore.sh
+++ test_scripts/test_backup_restore.sh
@@ -18,74 +18,73 @@ ORIGINAL_RONDB_NAMESPACE=rondb-original
 RESTORED_RONDB_NAMESPACE=rondb-restored
 RONDB_CLUSTER_NAME=my-rondb
 
-_getBackupId() {
-    POD_NAME=$(kubectl get pods -n $ORIGINAL_RONDB_NAMESPACE --selector=job-name=manual-backup -o jsonpath='{.items[?(@.status.phase=="Succeeded")].metadata.name}' | head -n 1)
-    BACKUP_ID=$(kubectl logs $POD_NAME -n $ORIGINAL_RONDB_NAMESPACE --container=upload-native-backups | grep -o "BACKUP-[0-9]\+" | head -n 1 | awk -F '-' '{print $2}')
+getBackupId() {
+    local namespace=$1
+    POD_NAME=$(kubectl get pods -n $namespace --selector=job-name=manual-backup -o jsonpath='{.items[?(@.status.phase=="Succeeded")].metadata.name}' | head -n 1)
+    BACKUP_ID=$(kubectl logs $POD_NAME -n $namespace --container=upload-native-backups | grep -o "BACKUP-[0-9]\+" | head -n 1 | awk -F '-' '{print $2}')
     echo $BACKUP_ID
 }
 
+destroy_cluster() {
+    local namespace=$1
+    helm delete $RONDB_CLUSTER_NAME -n $namespace || true
+    kubectl delete namespace $namespace || true
+}
+
 setupFirstCluster() {
-    kubectl delete namespace $ORIGINAL_RONDB_NAMESPACE || true
-    kubectl create namespace $ORIGINAL_RONDB_NAMESPACE
+    local namespace=$1
+    kubectl delete namespace $namespace || true
+    kubectl create namespace $namespace
 
     kubectl create secret generic $BUCKET_SECRET_NAME \
-        --namespace=$ORIGINAL_RONDB_NAMESPACE \
+        --namespace=$namespace \
         --from-literal "key_id=${MINIO_ACCESS_KEY}" \
         --from-literal "access_key=${MINIO_SECRET_KEY}"
 
     helm install $RONDB_CLUSTER_NAME \
-        --namespace=$ORIGINAL_RONDB_NAMESPACE \
+        --namespace=$namespace \
         --values ./values/minikube/mini.yaml \
         --values $backups_values_file \
         --set backups.enabled=true .
 
-    helm test -n $ORIGINAL_RONDB_NAMESPACE $RONDB_CLUSTER_NAME --logs --filter name=generate-data
+    helm test -n $namespace $RONDB_CLUSTER_NAME --logs --filter name=generate-data
 
-    kubectl delete job -n $ORIGINAL_RONDB_NAMESPACE manual-backup || true
-    kubectl create job -n $ORIGINAL_RONDB_NAMESPACE --from=cronjob/create-backup manual-backup
-    bash .github/wait_job.sh $ORIGINAL_RONDB_NAMESPACE manual-backup 180
-    BACKUP_ID=$(_getBackupId)
+    kubectl delete job -n $namespace manual-backup || true
+    kubectl create job -n $namespace --from=cronjob/create-backup manual-backup
+    bash .github/wait_job.sh $namespace manual-backup 180
+    BACKUP_ID=$(getBackupId $namespace)
     echo "BACKUP_ID is ${BACKUP_ID}"
-}
-
-destroy_first_cluster() {
-    helm delete $RONDB_CLUSTER_NAME -n $ORIGINAL_RONDB_NAMESPACE  || true
-    kubectl delete namespace $ORIGINAL_RONDB_NAMESPACE  || true
 }
 
 restoreCluster() {
-    BACKUP_ID=$(_getBackupId)
-    echo "BACKUP_ID is ${BACKUP_ID}"
+    local namespace=$1
+    local backupId=$2
 
-    destroy_first_cluster
-
-    kubectl delete namespace $RESTORED_RONDB_NAMESPACE || true
-    kubectl create namespace $RESTORED_RONDB_NAMESPACE
+    kubectl delete namespace $namespace || true
+    kubectl create namespace $namespace
 
     kubectl create secret generic $BUCKET_SECRET_NAME \
-        --namespace=$RESTORED_RONDB_NAMESPACE \
+        --namespace=$namespace \
         --from-literal "key_id=${MINIO_ACCESS_KEY}" \
         --from-literal "access_key=${MINIO_SECRET_KEY}"
 
     helm install $RONDB_CLUSTER_NAME \
-        --namespace=$RESTORED_RONDB_NAMESPACE \
+        --namespace=$namespace \
         --values ./values/minikube/mini.yaml \
         --values $restore_values_file \
-        --set restoreFromBackup.backupId=${BACKUP_ID} \
+        --set restoreFromBackup.backupId=${backupId} \
         .
 
     # Check that restoring worked
-    helm test -n $RESTORED_RONDB_NAMESPACE $RONDB_CLUSTER_NAME --logs --filter name=verify-data
+    helm test -n $namespace $RONDB_CLUSTER_NAME --logs --filter name=verify-data
 }
 
-destroy_restored_cluster() {
-    helm delete $RONDB_CLUSTER_NAME -n $RESTORED_RONDB_NAMESPACE  || true
-    kubectl delete namespace $RESTORED_RONDB_NAMESPACE  || true
-}
+setupFirstCluster $ORIGINAL_RONDB_NAMESPACE
+BACKUP_ID=$(getBackupId $ORIGINAL_RONDB_NAMESPACE)
+destroy_cluster $ORIGINAL_RONDB_NAMESPACE
 
-setupFirstCluster
-restoreCluster
-destroy_restored_cluster
+restoreCluster $RESTORED_RONDB_NAMESPACE $BACKUP_ID
+destroy_cluster $RESTORED_RONDB_NAMESPACE
 
 rm -f $backups_values_file
 rm -f $restore_values_file
