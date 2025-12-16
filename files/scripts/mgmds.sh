0983baba63b4a7c31fc482ea68978f9855256691
Axel Svensson <axel@hopsworks.ai>
2025-12-16 18:27:42 +0100

    [RONDB-982] Write stdout from mgmds,ndbmtds,mysqld to log file

--- files/scripts/mgmds.sh
+++ files/scripts/mgmds.sh
@@ -2,14 +2,18 @@
 
 # Copyright (c) 2024-2025 Hopsworks AB. All rights reserved.
 
+set -euo pipefail
+
 # The configuration database is a binary representation of the cluster's configuration.
 # It is essentially the config.ini including all changes that were applied via the
 # management client (ndb_mgm).
 
 # We're always doing an initial start so that changes in the config.ini are applied
 # Even if the main container starts, it will have the newest config.ini mounted to it.
+
 ndb_mgmd --initial \
     --nodaemon \
     --ndb-nodeid=65 \
     -f "$RONDB_DATA_DIR/config.ini" \
-    --configdir="{{ include "rondb.dataDir" $ }}/mgmd"
+    --configdir="{{ include "rondb.dataDir" $ }}/mgmd" 2>&1 \
+    | tee "${LOG_DIR}/ndb_mgmd.log"
