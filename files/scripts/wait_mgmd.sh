536bd5488988860c713e6c07ec1106dd9e9cb26a
Salman Niazi <salman.nz@gmail.com>
2026-01-21 14:47:14 +0100

    RONDB-995: Release RonDB 24.10.18 (#147)
    * RONDB-995: Release RonDB 24.10.18
    
    * RONDB-995: Copyright header fixes
    
    * made scripts executable

--- files/scripts/wait_mgmd.sh
+++ files/scripts/wait_mgmd.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Copyright (c) 2024-2025 Hopsworks AB. All rights reserved.
+# Copyright (c) 2024-2026 Hopsworks AB. All rights reserved.
+
+
 
 until nslookup $MGMD_HOSTNAME; do
     echo "Waiting for $MGMD_HOSTNAME to be resolvable..."
